837085d0f7cfb60baeb7e54da4c4a79e99a08efb
Leslie Stevens <flymaven@protonmail.com>
2019-07-04 15:15:01 -0500

    add bcrypt gem and custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   resources :locations
   resources :reagent_uses
   resources :reagents
