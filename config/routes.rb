382fcf5ba56fe4b49efdbad70f354786964a3db8
Leslie Stevens <flymaven@protonmail.com>
2019-07-06 15:33:50 -0500

    add new form for reagents, add nested routes for reagents

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   resources :reagents
   resources :users
   resources :labs
+  resources :labs, only: [:show] do
+    resources :reagents, only: [:index, :new, :create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
