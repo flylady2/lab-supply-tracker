7fedaa28b8cfc394b44126452e16d290c4b3a20f
Leslie Stevens <flymaven@protonmail.com>
2019-07-06 12:31:50 -0500

    build new and create methods for users and labs

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,2 +1,16 @@
 class LabsController < ApplicationController
+
+private
+
+def new
+  @lab = Lab.new
+render
+
+def create
+  @lab = Lab.new(lab_params)
+end
+
+def lab_params
+  params.require(:lab).permit(:principal_investigator, :location)
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+#def index
+  #@trainers = Trainer.all
+#end
+
+#loading the signup form
+def new
+  @user = User.new
+end
+
+#signup
+def create
+  #byebug
+  if params[:lab_id] && lab = Lab.find_by_id(params[:lab_id])
+    @user = lab.build_user(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_home_path(@user)
+    else
+      render :new
+    end
+  else
+    #flash[:message] = "You must be a member of a participating lab to sign up"
+    redirect_to '/' #home or welcome page
+  end
+end
+
+
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :lab_id, :admin)
+  end
 end
