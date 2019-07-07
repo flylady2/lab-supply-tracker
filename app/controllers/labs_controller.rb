74704b381a7c09c115027e328e81d44b6f227194
Leslie Stevens <flymaven@protonmail.com>
2019-07-07 16:26:44 -0500

    build labs routes and views

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,16 +1,23 @@
 class LabsController < ApplicationController
 
-private
+
 
 def new
   @lab = Lab.new
-render
+  render :new
+end
 
 def create
   @lab = Lab.new(lab_params)
 end
 
+def show
+  @lab = Lab.find_by_id(params[:id])
+end
+
+private
 def lab_params
   params.require(:lab).permit(:principal_investigator, :location)
 end
+
 end
