12e25f67c9196e3e43c198033b20fec483de5da8
Leslie Stevens <flymaven@protonmail.com>
2019-07-07 16:27:31 -0500

    build reagents routes and methods

--- app/controllers/reagents_controller.rb
+++ app/controllers/reagents_controller.rb
@@ -1,2 +1,34 @@
 class ReagentsController < ApplicationController
+
+  def index
+    #nested routes
+    if params[:lab_id] && lab = Lab.find_by_id(params[:lab_id])
+      @reagents = lab.reagents
+    else
+      redirect_to '/'
+    end
+  end
+
+  def new
+    #nested route
+    if params[:lab_id] && lab = Lab.find_by_id(params[:lab_id])
+      @reagent = lab.reagents.build
+    else
+      redirect_to '/'
+    end
+  end
+
+  def create
+    @reagent = lab.reagents.build(reagent_params)
+    if @reagent.save
+      redirect_to lab_reagent_path(@reagent)
+    else
+      render :new
+    end
+  end
+
+  private
+  def reagent_params(*args)
+    params.require(:reagent).permit(*args)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   resources :users
   resources :labs
   resources :labs, only: [:show] do
-    resources :reagents, only: [:index, :new, :create]
+    resources :reagents
+    resources :locations
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
