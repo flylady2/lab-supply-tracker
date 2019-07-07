bb2abdbe78915c3ea44751b1747e70703a02cc99
Leslie Stevens <flymaven@protonmail.com>
2019-07-07 16:25:58 -0500

    build location routes and views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,34 @@
 class LocationsController < ApplicationController
+
+  def index
+    #nested routes
+    if params[:lab_id] && lab = Lab.find_by_id(params[:lab_id])
+      @locations = lab.locations
+    else
+      redirect_to '/'
+    end
+  end
+
+  def new
+    #nested route
+    if params[:lab_id] && lab = Lab.find_by_id(params[:lab_id])
+      @location = lab.locations.build
+    else
+      redirect_to '/'
+    end
+  end
+
+  def create
+    @location = lab.locations.build(location_params)
+    if @location.save
+      redirect_to lab_location_path(@location)
+    else
+      render :new
+    end
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(location_params)
+  end
 end
