ebeef9497d092fdb24e5e436321bc38420d01567
Leslie Stevens <flymaven@protonmail.com>
2019-07-09 15:46:31 -0500

    reset database

--- db/migrate/20190704185641_create_labs.rb
+++ db/migrate/20190704185641_create_labs.rb
@@ -2,7 +2,7 @@ class CreateLabs < ActiveRecord::Migration[5.2]
   def change
     create_table :labs do |t|
       t.string :principal_investigator
-      t.string :location
+      t.string :institution
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2019_07_04_191531) do
 
   create_table "labs", force: :cascade do |t|
     t.string "principal_investigator"
-    t.string "location"
+    t.string "institution"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
