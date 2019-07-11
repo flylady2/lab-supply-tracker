4b07e46a813345e44615024dee9722fc08470211
Leslie Stevens <flymaven@protonmail.com>
2019-07-11 14:46:14 -0500

    build #enough for reagent_use model

--- app/models/reagent_use.rb
+++ app/models/reagent_use.rb
@@ -1,4 +1,15 @@
 class ReagentUse < ApplicationRecord
-  belongs_to :Reagent
-  belongs_to :User
+  belongs_to :reagent
+  belongs_to :user
+
+  def enough
+    if self.quantity > self.reagent.quantity
+      "Sorry. There is not enough of this reagent for your experiment."
+    else
+      self.reagent.update(
+        :quantity => self.reagent.quantity - self.quantity
+      )
+      "Thanks for registering your reagent use!"
+    end
+  end
 end
