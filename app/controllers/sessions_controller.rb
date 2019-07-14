05c99dfb2db3244793cd18c9d06a29af01de8eb0
Leslie Stevens <flymaven@protonmail.com>
2019-07-14 17:18:32 -0500

    add partial  to limit lab view to lab user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ def create
   @user = User.find_by(email: params[:user][:email])
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    #byebug
     redirect_to lab_path(@user.lab_id)
   else
     redirect_to 'login'
