fe8a2098c85dc835265bc75df44acdc3329df479
Leslie Stevens <flymaven@protonmail.com>
2019-07-21 17:14:03 -0500

    add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :reagents
   validates :name, presence: true
+  validates :name, uniqueness: true
 
   #scope :reagents_by_lab, -> ()
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ApplicationRecord
   belongs_to :lab
   has_many :reagents
 
+  validates :name, presence: true
   validates :name, uniqueness: { scope: :lab_id, message: "Lab already has this location."}
 
 
