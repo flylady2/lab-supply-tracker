dfc01fb9e8c9feeb22fbfea4a146c41a9c9497e7
Leslie Stevens <flymaven@protonmail.com>
2019-08-05 11:00:32 -0500

    add bootstrap gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,23 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   private
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 
-  def logged_in
-    !!current_user
-  end
 
-  def require_login
-    unless logged_in
-      flash[:message] = "You must be logged in to use the app."
-      redirect_to '/login'
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
     end
-  end
+
+
+      def logged_in
+        !!current_user
+      end
+
+      def require_login
+        unless logged_in
+          flash[:message] = "You must be logged in to use the app."
+          redirect_to '/login'
+        end
+      end
 
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+
   def go_home
     @lab = Lab.find(@current_user.lab_id)
     link_to "Lab Home Page", lab_path(@lab)
