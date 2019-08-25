aa919a8deb69b31fb33c42c044fa461a4bac0aca
Leslie Stevens <flymaven@protonmail.com>
2019-08-25 11:24:22 -0500

    fix reagent_use filters

--- app/controllers/reagent_uses_controller.rb
+++ app/controllers/reagent_uses_controller.rb
@@ -6,55 +6,44 @@ class ReagentUsesController < ApplicationController
 #  end
 
   def index
-    byebug
+    #byebug
     if params[:lab_id] && @lab = Lab.find_by_id(params[:lab_id])
       @reagent_uses = @lab.reagent_uses
       if params[:reagent]
         reagent = @lab.reagents.search_by_name(params[:reagent]).first
-
+        #byebug
         if reagent
           @reagent_uses = ReagentUse.search_by_reagent_id(reagent.id)
+          #byebug
         else
           flash[:message] = "There are no reagent use records for that reagent."
           @reagent_uses = @lab.reagent_uses
         end
-      end
 
-    if params[:user]
+      elsif params[:user]
         user = @lab.users.search_by_user_name(params[:user]).first
         if user
           #byebug
           @reagent_uses = ReagentUse.search_by_user_id(user.id)
         else
           flash[:message] = "There are no reagent use records for that user."
-              @reagent_uses = @lab.reagent_uses
-          #redirect_to lab_path(@lab)
+          @reagent_uses = @lab.reagent_uses
         end
-      end
 
-    if params[:start_date] && params[:end_date]
+      elsif params[:start_date] && params[:end_date]
         #byebug
-        @reagent_uses = @lab.reagent_uses.search_by_date(params[:start_date], params[:end_date])
-        if @reagent_uses
-          @reagent_uses
+        reagent_uses = @lab.reagent_uses.search_by_date(params[:start_date], params[:end_date])
+        if reagent_uses
+          @reagent_uses = reagent_uses
         else
           flash[:message] = "There are no reagent use records for that time period."
           @reagent_uses = @lab.reagent_uses
         end
-      else
-        @reagent_uses = @lab.reagent_uses
       end
-
-        #@reagent_uses = @lab.reagent_uses
-      #end
-
-      #else
-
-      #  @reagent_uses = @lab.reagent_uses
-    #  end
     else
       @reagent_uses = ReagentUse.all
     end
+    #byebug
   end
 
 
