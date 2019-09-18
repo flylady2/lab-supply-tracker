7863ebbb65d7d2f5986756890a72ddaec5df8b97
Leslie Stevens <flymaven@protonmail.com>
2019-09-18 15:10:22 -0500

    move category_array method from reagents_helper to labs_helper

--- app/helpers/labs_helper.rb
+++ app/helpers/labs_helper.rb
@@ -1,2 +1,11 @@
 module LabsHelper
+
+  def category_array
+    category_array = []
+    @lab.reagents.each do |reagent|
+      category_array << reagent.category
+    end
+    category_array.uniq
+  end
+
 end
