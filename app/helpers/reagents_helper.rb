21b7bf7e4f4262f1585804f62b8dd06146022b17
Leslie Stevens <flymaven@protonmail.com>
2019-09-18 15:12:10 -0500

    removed category_array method to labs_helper

--- app/helpers/reagents_helper.rb
+++ app/helpers/reagents_helper.rb
@@ -1,12 +1,5 @@
 module ReagentsHelper
 
-  def category_array
-    category_array = []
-    @lab.reagents.each do |reagent|
-      category_array << reagent.category
-    end
-    category_array.uniq
-  end
 
 
 end
